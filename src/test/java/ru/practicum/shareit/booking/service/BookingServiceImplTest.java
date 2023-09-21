02ef93e0464092df6e08bcb6966a668986796b4c
Veber <pavelbo92@yandex.ru>
2023-09-21 14:55:18 +0300

    fix: add-item-requests Изменены тесты

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -19,7 +19,6 @@ import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
-import ru.practicum.shareit.validators.EndAfterStartValidator;
 
 import java.time.LocalDateTime;
 import java.util.List;
