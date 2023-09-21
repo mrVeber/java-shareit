2299afd1b67656f9abe6bf4deff5f37ee8c58074
Veber <pavelbo92@yandex.ru>
2023-09-21 17:37:32 +0300

    fix: add-item-requests Изменены тесты

--- src/main/java/ru/practicum/shareit/exception/model/WrongNumbersForPagingException.java
+++
@@ -1,7 +0,0 @@
-//package ru.practicum.shareit.exception.model;
-//
-//public class WrongNumbersForPagingException extends RuntimeException {
-//    public WrongNumbersForPagingException(String message) {
-//        super(message);
-//    }
-//}
